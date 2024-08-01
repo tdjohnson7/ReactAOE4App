be0ef152a167addd401e9590522658d76839e03f
tdjohnson7 <tdjohnson7@msn.com>
2024-08-01 17:30:30 -0500

    commit

--- DUPvite.config.js
+++ DUPvite.config.js
@@ -1,28 +1,28 @@
-// // import { defineConfig } from 'vite'
-// // import react from '@vitejs/plugin-react'
-
-// // // https://vitejs.dev/config/
-// // export default defineConfig({
-// //   plugins: [react()],
-// // })
-
-
 // import { defineConfig } from 'vite'
 // import react from '@vitejs/plugin-react'
 
 // // https://vitejs.dev/config/
 // export default defineConfig({
 //   plugins: [react()],
-//   build: {
-//     manifest: true,
-//     rollupOptions: {
-//       input: './src/main.jsx',
-//     },
-//   },
-//   server: {
-//     proxy: {
-//       "/api": "http://localhost:8001/", // the address that u serve in the backend 
-//       // "/api": "http://127.0.0.1:5173/", // the address that u serve in the backend 
-//     },
-//   },
 // })
+
+
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    manifest: true,
+    rollupOptions: {
+      input: './src/main.jsx',
+    },
+  },
+  server: {
+    proxy: {
+      "/api": "http://localhost:8001/", // the address that u serve in the backend 
+      // "/api": "http://127.0.0.1:5173/", // the address that u serve in the backend 
+    },
+  },
+})
