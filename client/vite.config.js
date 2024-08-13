1e6bdf54b5b040b7c1d3764eee010a394451d208
tdjohnson7 <tdjohnson7@msn.com>
2024-08-12 23:38:27 -0500

    commit

--- client/vite.config.js
+++ client/vite.config.js
@@ -13,6 +13,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/client/',
   build: {
     manifest: true,
     rollupOptions: {
