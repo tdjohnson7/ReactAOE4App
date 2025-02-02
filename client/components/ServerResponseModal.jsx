ee93177966fe0af94339baa7f18f12863b67bf55
tdjohnson7 <tdjohnson7@msn.com>
2025-02-01 20:29:25 -0600

    commit

--- client/components/ServerResponseModal.jsx
+++ client/components/ServerResponseModal.jsx
@@ -2,8 +2,8 @@
 
 // export default function ServerResponseModal(props) {
 //   // console.log("response", response)
-//   return ( 
-//       props.response ? 
+//   return (
+//       props.response ?
 //         <div>
 //            {/* <h1 className="text-3xl font-bold  underline">ServerResponseModal</h1> */}
 //           <h2>{props.response.winningUnit.name}</h2>
@@ -26,9 +26,9 @@ import { Modal, Button } from "react-bootstrap";
 
 const ServerResponseModal = (props) => {
   // Create a state variable to control the modal visibility
-  let response = props.response
-  console.log('response from modal', props.response)
-  console.log('props',props)
+  let response = props.response;
+  console.log("response from modal", props.response);
+  console.log("props", props);
 
   // const [showModal, setShowModal] = useState(true)
 
@@ -41,23 +41,36 @@ const ServerResponseModal = (props) => {
 
   // Render the modal using the <dialog> element
   // const renderModal = (props) => {
-    
-    return (  
-      <div>        
-        {props.showModal && <Modal.Dialog >
-            {props.response.winningUnit ? winningLosingUnitResponse(response= {response}) : <p>{props.response.tieStatement}</p>}  
 
-            <Button size="sm" type="button" variant="outline-light" onClick={() => {props.setShowModal(false), console.log('showModal', props.showModal)}}>Close</Button>
-        </Modal.Dialog>}
-        </div>
-      
+  return (
+    <>
+      {props.showModal && (
+        <Modal.Dialog>
+          {props.response.winningUnit ? (
+            winningLosingUnitResponse((response = { response }))
+          ) : (
+            <p>{props.response.tieStatement}</p>
+          )}
 
-    ); 
+          <Button
+            size="sm"
+            type="button"
+            variant="outline-light"
+            onClick={() => {
+              props.setShowModal(false),
+                console.log("showModal", props.showModal);
+            }}
+          >
+            Close
+          </Button>
+        </Modal.Dialog>
+      )}
+    </>
+  );
   // };
-}
+};
 
-  // Return the modal element
-  // return <div>{renderModal()}</div>;
-
+// Return the modal element
+// return <div>{renderModal()}</div>;
 
 export default ServerResponseModal;
--- server/test.js
+++
@@ -1,3 +0,0 @@
-console.log(process.env)
-console.log(process.env.DEB_STRING)
-dbConnectionString.startsWith('mongodb+srv://')
